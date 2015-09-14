15ea57e25c7d8c196d9cbcdbb374f92e2199ea9c
Henrique Dias <heukirne@gmail.com>
2015-09-14 11:26:49 -0300

    fix forever

--- tools/unix/forever.sh
+++ tools/unix/forever.sh
@@ -1,2 +1,2 @@
 localdir="$PWD"
-sudo forever start -a -l $localdir/deploy/server/log.log -e $localdir/deploy/server/err.log -o $localdir/deploy/server/out.log --workingDir $localdir/deploy/server $localdir/deploy/server/app.js -vvvv
+sudo forever start -a -l $localdir/deploy/server/log.log -e $localdir/deploy/server/err.log -o $localdir/deploy/server/out.log --workingDir $localdir/deploy/server --uid "assimp" $localdir/deploy/server/app.js -vvvv
