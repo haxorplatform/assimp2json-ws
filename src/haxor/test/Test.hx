3a494df6b9cd44a7e51e82fd818382fb2f6ca691
Henrique Dias <heukirne@gmail.com>
2015-09-16 08:03:54 -0300

    init unit tests

---
+++ src/haxor/test/Test.hx
@@ -0,0 +1,28 @@
+// from https://github.com/dionjwa/nodejs-std/blob/master/test/Test.hx
+package haxor.test;
+
+import sys.FileSystem;
+import haxe.unit.TestRunner;
+
+class Test
+{
+
+	static function main()
+	{
+		untyped TestRunner.print = console.log;
+
+		if (haxe.macro.Compiler.getDefine("nodejs") != "1") {
+			throw "nodejs compiler flag not defined";
+		}
+
+		var runner = new TestRunner();
+		//runner.add(new TestSys());
+		//runner.add(new TestNode());
+		//runner.add(new TestBugs());
+		// your can add others TestCase here
+
+		// Run them and and exit with the right return code
+		var success = runner.run();
+		(untyped process).exit(success ? 0 : 1);
+	}
+}
