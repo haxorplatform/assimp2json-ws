a87b8676f87f9b123777f4993e93b96d8dc6d5e6
Henrique Dias <heukirne@gmail.com>
2015-09-18 11:09:33 -0300

    fix forever

--- tools/unix/forever.sh
+++ tools/unix/forever.sh
@@ -1,4 +1,4 @@
 cd deploy/server/
 localdir="$PWD"
 forever stop assimp
-sudo forever start -a -l $localdir/log.log -e $localdir/err.log -o $localdir/out.log --workingDir $localdir --uid "assimp" $localdir/app.js -vvvv
+forever start -a -l $localdir/log.log -e $localdir/err.log -o $localdir/out.log --workingDir $localdir --uid "assimp" $localdir/app.js -vvvv
